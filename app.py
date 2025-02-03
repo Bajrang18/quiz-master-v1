531dcedb65ca3e10a55cbfc4965a3288a9cc4736
Bajrang Kumar <bajrangsingh1804@gmail.com>
2025-02-03 20:59:30 +0530

    Update

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-def main():
-    print("Hello, World!")
+# def main():
+#     print("Hello, World!")
+
+# if __name__ == "__main__":
+#     main()
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello World
+</body>
+</html>"""
 
 if __name__ == "__main__":
-    main()
+    app.debug=True
+    app.run()
+
