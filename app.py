aaf8b06d74da6af4b10c7cfa2f0f986f358ad5de
Bajrang Kumar <bajrangsingh1804@gmail.com>
2025-02-03 21:03:44 +0530

    Update

--- app.py
+++ app.py
@@ -7,18 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hello World
-</body>
-</html>"""
+    return 
 
 if __name__ == "__main__":
     app.debug=True
