032be0820d01f8374b192b7e30332bdc6b0d166e
Bajrang Kumar <bajrangsingh1804@gmail.com>
2025-02-05 02:09:03 +0530

    created basic login

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-# def main():
-#     print("Hello, World!")
-
-# if __name__ == "__main__":
-#     main()
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return render_template("file1.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.debug=True
