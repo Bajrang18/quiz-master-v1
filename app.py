c664a48a8aedf91aea43feb8f0f730e6f775e1f4
Bajrang Kumar <bajrangsingh1804@gmail.com>
2025-02-06 15:19:36 +0530

    flask login

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+
+import config
+
+import routes
 
 if __name__ == "__main__":
-    app.debug=True
-    app.run()
+    app.run(debug=True)
 
-    
 
+# # config.py
+# from dotenv import load_dotenv
+# import os
+# from app import app
+
+# load_dotenv()
+
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+
+
+# # routes.py
+# from flask import render_template
+# from app import app
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
